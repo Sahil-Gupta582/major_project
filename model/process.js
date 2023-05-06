abd863a5b6d09826ccde56316f65986effdc25a0
Sahil gupta <gupta.sahil@naukri.com>
2023-05-06 22:05:40 +0530

    dbConnection done

--- model/process.js
+++ model/process.js
@@ -1,5 +1,30 @@
-const {DataTypes, Sequelize} = require('sequelize');
+const Sequelize = require('sequelize');
+const sequelize = require('./../dbConnection')
 
-const Process = Sequelize.define('Process', {
-    
-})
+const Process = sequelize.define('Process', {
+    task_name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    task_priority:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    task_duration:{
+        type: Sequelize.FLOAT,
+        allowNull: false,
+    }, 
+    task_energy:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    task_depends_on:{
+        type: Sequelize.JSON,
+        allowNull: true,
+    }
+});
+
+module.exports = {
+    Process,
+}
