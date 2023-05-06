653c80b4459a40ff5b16916e7b88c912e0d8b186
Kashyapdevesh <kashyapdevesh30@gmail.com>
2023-05-07 02:48:43 +0530

    populate json script added

--- populate_json.py
+++ populate_json.py
@@ -1,18 +1,26 @@
 import json
 import time
 import random
+import os
 
 task_id = 1
 
-while True:
-    data = {f'job{task_id}': [round(random.uniform(0.1, 2.9), 1), round(random.uniform(0.1, 5), 1), min(task_id - 1, random.randint(0, 3))]}
-    filename = "new_tasks.json"
+filename = "new_tasks.json"
 
-    try:
-    	with open(filename,'r') as file:
-    		existing_data=json.load(file)
-    except FileNotFoundError:
-    	existing_data={}
+while True:
+    dependencies_count = min(task_id - 1, random.randint(0, 3))
+    dependencies = list(set(random.sample(range(1, task_id), dependencies_count)))
+
+    if len(list(dependencies))==0:
+    	dependencies=None
+
+    data = {f'job{task_id}': [round(random.uniform(0.1, 2.9), 1), round(random.uniform(0.1, 5), 1), dependencies]}
+
+    if os.path.isfile(filename) and os.path.getsize(filename) > 0:
+        with open(filename, 'r') as file:
+            existing_data = json.load(file)
+    else:
+        existing_data = {}
 
     existing_data.update(data)
 
@@ -23,4 +31,4 @@ while True:
 
     task_id += 1
 
-    time.sleep(5)
+    time.sleep(3)
